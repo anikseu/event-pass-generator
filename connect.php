1d5da5cfa432d9c8c260c46e06eb78e2f39290a0
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-01-12 20:59:43 +0600

    Update connect.php
    Adding DB Info

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', ''); // Host, DB user, Password 
+$connection = mysqli_connect('sql11.freemysqlhosting.net', 'sql11215321', '1utQGWtGwi'); // Host, DB user, Password 
 
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
 
-$select_db = mysqli_select_db($connection, 'event'); // Database Name
+$select_db = mysqli_select_db($connection, 'sql11215321'); // Database Name
 
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
